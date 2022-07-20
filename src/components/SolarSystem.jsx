30100cd1550dcd7ed82d4fbce78d91e2e1ac3a61
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-07-20 16:46:46 -0300

    requisito 3 e 4 realizado

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Title from './Title';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
+
+    );
+  }
+}
+
+export default SolarSystem;
