353fc6a1b0d2b900f1dfeb122d4ea43b3bb4e4d7
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-07-20 19:14:44 -0300

    requisito 7 realizado

---
+++ src/components/Missions.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+/* import PropTypes from 'prop-types'; */
+
+class Missions extends Component {
+  render() {
+    return (
+
+      <div data-testid="missions">
+        Conteúdo
+      </div>
+
+    );
+  }
+}
+
+/* Missions.propTypes = {
+  xxxx: PropTypes.string.isRequired,
+}; */
+
+export default Missions;
