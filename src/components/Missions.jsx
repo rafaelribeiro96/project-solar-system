ab29240bbbe1020bb8a2f58775304dadd3f73518
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-07-20 20:18:12 -0300

    requisito 10 realizado

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import Title from './Title';
-/* import PropTypes from 'prop-types'; */
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
+    const mission = missions
+      .map(({ name, year, country, destination }, index) => (<MissionCard
+        Key={ index }
+        name={ name }
+        year={ year }
+        country={ country }
+        destination={ destination }
+
+      />));
     return (
 
       <div data-testid="missions">
         <Title headline="Missões" />
+        <p>{mission}</p>
       </div>
 
     );
   }
 }
-
-/* Missions.propTypes = {
-  xxxx: PropTypes.string.isRequired,
-}; */
 
 export default Missions;
